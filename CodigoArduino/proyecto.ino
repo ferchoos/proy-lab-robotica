f68ce6e8d2377d6a168354f2acf2c55125a9973f
TeoGigena <164062603+TeoGigena@users.noreply.github.com>
2024-11-06 10:53:54 -0300

    Update proyecto.ino

--- CodigoArduino/proyecto.ino
+++ CodigoArduino/proyecto.ino
@@ -4,17 +4,15 @@
 #define DHTPIN 2       
 #define DHTTYPE DHT11  
 #define FAN_PIN 9      
-#define STATE_PIN 2    
 
 DHT dht(DHTPIN, DHTTYPE);
 SoftwareSerial bluetooth(10, 11); 
 
-int manualFanSpeed = -1; 
+int manualFanSpeed = -1; // -1 for automatic mode
 bool fanEnabled = true;  
 
 void setup() {
     pinMode(FAN_PIN, OUTPUT);
-    pinMode(STATE_PIN, INPUT);
     Serial.begin(9600);
     bluetooth.begin(9600);
     dht.begin(); 
@@ -23,37 +21,33 @@ void setup() {
 void loop() {
     
     float temperature = dht.readTemperature();
-    float humidity = dht.readHumidity();
 
-    
     if (bluetooth.available()) {
         char command = bluetooth.read();
-        if (command == 'A') {
-            manualFanSpeed = -1; 
-            fanEnabled = true;   
+        if (command == 'A') {             // AUTOMATICO
+            manualFanSpeed = -1;         
+            fanEnabled = true;
             Serial.println("Automatic control mode");
-        } else if (command >= '0' && command <= '9') {
-            manualFanSpeed = map(command - '0', 0, 9, 0, 255); 
-            fanEnabled = true;   
-            Serial.print("Manual control mode, fan speed: ");
-            Serial.println(manualFanSpeed);
-        } else if (command == 'O') {
-            fanEnabled = false;  
+        } else if (command == 'M') {      // MAXIMO
+            manualFanSpeed = 255;         
+            fanEnabled = true;
+            Serial.println("Max speed mode");
+        } else if (command == 'L') {      // BAJO
+            manualFanSpeed = 64;          
+            fanEnabled = true;
+            Serial.println("Low speed mode");
+        } else if (command == 'O') {      // APAGAR
+            fanEnabled = false;           
             Serial.println("Fan turned off");
-        } else if (command == 'E') {
-            fanEnabled = true;   
-            Serial.println("Fan enabled");
         }
     }
 
-    
-    int fanSpeed = 0; 
+    int fanSpeed = 0;
     if (fanEnabled) {
         fanSpeed = (manualFanSpeed == -1) ? autoControlFanSpeed(temperature) : manualFanSpeed;
     }
     analogWrite(FAN_PIN, fanSpeed);
 
-    
     Serial.print("Temperature: ");
     Serial.print(temperature);
     Serial.print("°C, Fan Speed: ");
@@ -63,7 +57,6 @@ void loop() {
 }
 
 int autoControlFanSpeed(float temperature) {
-    
     if (isnan(temperature)) {
         return 0; 
     }
@@ -76,3 +69,4 @@ int autoControlFanSpeed(float temperature) {
         return 255; 
     }
 }
+
